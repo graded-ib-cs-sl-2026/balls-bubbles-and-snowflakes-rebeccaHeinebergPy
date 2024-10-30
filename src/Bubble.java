5ca1e0464738e42db5410b1d5503ebbe231c58f0
Rebecca <No>
2024-10-30 09:38:58 -0300

    bubbles done

--- src/Bubble.java
+++ src/Bubble.java
@@ -1,10 +1,49 @@
-/** THE ENTIRE CLASS IS INSIDE A COMMENT RIGHT NOW. TO ENABLE THE CLASS, DELETE THIS LINE AND THE LAST ONE.
 
 class Bubble {
 
     //MISSING MANY INSTANCE VARIABLES
+    private Sketch s;
+    /** the radius of the bubble */
+    private float radius;
+    private float x;
+    private float y;
+    /** The number of pixels the bubbles moves right per frame */
+    private float xSpeed;
+    /** The number of pixels the bubbles moves down per frame */
+    private float ySpeed;
+    /** The color of the inside of the bubbles */
+    private int fillColor;
+    /** The color of the outside of the bubble */
+    private int borderColor;
 
     //MISSING CONSTRUCTORS
+    public Bubble(Sketch sketch) {
+        s = sketch;
+        radius = 30;
+        x = 100;
+        y = 100;
+        xSpeed = 2;
+        ySpeed = -1;
+        fillColor = 0x00ffffff;
+        borderColor = s.color(0, 0, 0);
+    }
+
+    /** Fully specified constructor to allow changes to size, position, speed */
+    /** Does NOT allow changing color! Need to use setColors() for that. */
+    public Bubble(Sketch sketch, float radius, float x, float y, float xspeed, float yspeed) {
+        this.s = sketch;
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xspeed;
+        this.ySpeed = yspeed;
+
+    }
+
+    public void setColors(int fill, int border) {
+        borderColor = border;
+        fillColor = fill;
+    }
 
     // accessors for the radius, diameter, x, and y values 
     public float getRadius() {
@@ -44,4 +83,4 @@ class Bubble {
     }
 
 }
- **/
+ 
--- src/Sketch.java
+++ src/Sketch.java
@@ -2,11 +2,18 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 
-    /** Represents one ball */
+    /** BALLS */
     private Ball ball1;
     private Ball ball2; 
     private Ball ball3; 
     private Ball ball4; 
+
+    // BUBBLES
+    private Bubble bubble1; 
+    private Bubble bubble2; 
+    private Bubble bubble3; 
+    private Bubble bubble4; 
+
 
     /**
      * This method can only be used to change the window size. It runs before the
@@ -21,6 +28,8 @@ public class Sketch extends PApplet {
      * this to initialize the sketch.
      */
     public void setup() {
+
+    // BALLS: 
         ball1 = new Ball(this, random(1, 40), random(0, 500), random(0, 500), random(1, 10), random(1, 10));
         ball1.setColors(color(random (0, 255), random(0, 225), random(0, 255)), color(random (0, 255), random(0, 225), random(0, 255))); 
 
@@ -32,6 +41,18 @@ public class Sketch extends PApplet {
 
         ball4 = new Ball (this, 27, 97, 302, 2, 3); 
         ball4.setColors(color(0,255,2), 0); 
+
+    // BUBBLES
+        bubble1 = new Bubble (this); 
+
+        bubble2 = new Bubble (this, 10, 2, 30, 5, 10); 
+        bubble2.setColors(0x00ffffff, 0); 
+
+        bubble3 = new Bubble (this, 45, 499, 64, 1, 2); 
+        bubble3.setColors(0x00ffffff, 0); 
+
+        bubble4 = new Bubble (this, random(1, 50), random(0, 500), random(0, 500), random(1, 6), random(1, 6));
+        bubble4.setColors(0x00ffffff, 0); 
     }
 
     
@@ -44,6 +65,7 @@ public class Sketch extends PApplet {
         background(180, 180, 255);
         
 //BEHAVIORS
+    // BALLS
         ball1.move();
         ball1.draw();
 
@@ -55,9 +77,20 @@ public class Sketch extends PApplet {
 
         ball4.move(); 
         ball4.draw(); 
-    
-        
 
+    // BUBBLES
+
+        bubble1.move(); 
+        bubble1.draw(); 
+
+        bubble2.move(); 
+        bubble2.draw(); 
+
+        bubble3.move(); 
+        bubble3.draw(); 
+
+        bubble4.move(); 
+        bubble4.draw(); 
 
     }
 
